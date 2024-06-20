dd7056e67b6c80cbb65f443dcb50c0fc1c7d998e
Jeremy Chauvet <jer.chauvet@gmail.com>
2024-06-20 23:18:55 +0200

    feat: workflow improvement

---
+++ backend/core.py
@@ -0,0 +1,30 @@
+import random
+
+
+def generate_random_browser_headers():
+    user_agents = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15",
+        "Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1",
+        "Mozilla/5.0 (Linux; Android 11; SM-G991B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Mobile Safari/537.36"
+    ]
+
+    accept_languages = [
+        "en-US,en;q=0.9",
+        "en-GB,en;q=0.8,fr;q=0.6",
+        "fr-FR,fr;q=0.9,en;q=0.8",
+        "de-DE,de;q=0.9,en;q=0.8",
+        "es-ES,es;q=0.9,en;q=0.8"
+    ]
+
+    return {
+        "User-Agent": random.choice(user_agents),
+        "Accept-Language": random.choice(accept_languages),
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1"
+    }
--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,6 @@
+import json
+
+import core
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
@@ -13,40 +16,30 @@ def sanitize_date(date_str, date_format="%Y/%m/%d"):
         return None
 
 
-def get_html(url: str):
+def get_html(page_url: str):
     try:
-        response = requests.get(url)
-        #response.raise_for_status()  # Raise an exception for HTTP errors
+        response = requests.get(page_url, headers=core.generate_random_browser_headers())
+        match response.status_code:
+            case 200:
+                return BeautifulSoup(response.text, "html.parser")
+            case 403:
+                print(f"403 error trying to access {page_url}. Reason: {response.reason} ")
+                return None
     except requests.exceptions.RequestException as e:
         print(f"Error fetching the URL: {e}")
-    response = requests.get(url)
-    print(response)
-    match response.status_code:
-        case 200:
-            return BeautifulSoup(response.text, "html.parser")
-        case 403:
-            print(f"403 error trying to access {url}. Reason: {response.reason} ")
-            return None
 
 
-
-
-
-
-def get_publication_page_url(url: str):
-    soup = get_html(url)
+def get_publication_page_url(page_url: str):
+    soup = get_html(page_url)
     if soup is not None:
         soup.getText()
         link = soup.find("article").find("a", href=True)
-        print(f"https://www.legifrance.gouv.fr{link['href']}")
         return f"https://www.legifrance.gouv.fr{link['href']}"
 
 
-
-
-def get_publication_page_content(url: str) -> []:
+def get_publication_page_content(page_url: str) -> []:
     output = []
-    soup = get_html(url)
+    soup = get_html(page_url)
     if soup is not None:
         links = soup.find_all(class_="jorfLink")
         for link in links:
@@ -57,35 +50,35 @@ def get_publication_page_content(url: str) -> []:
                     "link": f"https://www.legifrance.gouv.fr{link["href"]}",
                 }
             )
+            break
         return output
 
 
-def get_publication_page_content_detail(url: str):
-    soup = get_html(url)
+def get_publication_page_content_detail(page_url: str):
+    output = []
+
+    soup = get_html(page_url)
     if soup is not None:
-        content_article: str = ""
-        name_article: str = ""
         try:
-            liste_sommaire = soup.find(id="liste-sommaire")
-            li_elements = liste_sommaire.find_all("li")
-            for li in li_elements:
-                article = li.find("article")
-                if article:
-                    p = article.find("p")
-                    if p:
-                        article_name = p.getText()
-                        print(f"{article_name} : ")
-                    div = article.find("div", class_="content")
-                    if div:
-                        article_content = div.getText()
-                        print(article_content)
-                        print("")
+            main_block = soup.find(id="liste-sommaire")
+            articles_block = main_block.find_all('article')
+            for article_block in articles_block:
+                article_number = article_block.find("p", {"class": "name-article"})
+                article_content = article_block.find("div", class_="content")
+
+                output.append({
+                    int(article_number.getText().replace("Article", "").strip()): article_content.getText(),
+                })
         except AttributeError as error:
             print(f"[ERROR] {error}")
-    return ""
+    return output
 
 
 if __name__ == "__main__":
+    # Variables
+    response = []
+
+
     # date = input("Type the date (YYYY/MM/DD): ")
     date = "2024/06/14"
     # sanitized_date = sanitize_date(date_input)
@@ -97,6 +90,11 @@ if __name__ == "__main__":
     if publication_page_url is not None:
         publication_page_content = get_publication_page_content(publication_page_url)
         for item in publication_page_content:
-            url = item["link"]
-            articles = get_publication_page_content_detail(url)
-            print(articles)
+            articles = get_publication_page_content_detail(item["link"])
+            # print(json.dumps(articles, indent=4))
+            item['articles'] = articles
+            response.append(item)
+
+            # response.append(publication_page_content.update())
+        print(json.dumps(response, indent=4))
+        print("----")
