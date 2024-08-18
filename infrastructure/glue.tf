9118ffc1251c3eb4c066c5764b6130d3cbac6280
Jeremy Chauvet <jer.chauvet@gmail.com>
2024-08-18 18:54:42 +0200

    feat: add glue data quality ruleset for column content.

--- infrastructure/glue.tf
+++ infrastructure/glue.tf
@@ -58,3 +58,14 @@ resource "aws_glue_catalog_table" "journal_officiel" {
 
   depends_on = [aws_glue_catalog_database.journal_officiel]
 }
+
+resource "aws_glue_data_quality_ruleset" "journal_officiel_rule_content" {
+  name    = "journal-officiel-rule-content"
+  description = "This rule checks tether text in column named 'content' is filled."
+  ruleset = "Rules = [ColumnValues \"content\" != NULL]"
+
+  target_table {
+    database_name = aws_glue_catalog_database.journal_officiel.name
+    table_name    = aws_glue_catalog_table.journal_officiel.name
+  }
+}
